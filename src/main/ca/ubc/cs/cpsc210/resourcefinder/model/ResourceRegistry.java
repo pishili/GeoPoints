e017fcbcc806a11a9379c905bd98a8e17d3a6a31
AutoBot <w-portal@cs210.ugrad.cs.ubc.ca>
2018-10-11 12:21:51 -0700

    Starter files

---
+++ src/main/ca/ubc/cs/cpsc210/resourcefinder/model/ResourceRegistry.java
@@ -0,0 +1,41 @@
+package ca.ubc.cs.cpsc210.resourcefinder.model;
+
+
+import java.util.*;
+
+// Registry of available resources
+public class ResourceRegistry {
+    private List<Resource> resources;
+
+    // EFFECTS: constructs empty resource registry
+    public ResourceRegistry() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: add resource to registry, maintains resources in the order added to registry
+    public void addResource(Resource resource) {
+        // stub
+    }
+
+    // EFFECTS: returns resources in registry as unmodifiable list (in order that they were added
+    // to registry)
+    public List<Resource> getResources() {
+        return null;  // stub
+    }
+
+    // EFFECTS: returns set of resources in registry that offer the given service
+    public Set<Resource> getResourcesOfferingService(Service service) {
+        return null;  // stub
+    }
+
+    // EFFECTS: returns set of resources in registry that offer all the services in requestedServices set
+    public Set<Resource> getResourcesOfferingAllServicesInSet(Set<Service> requestedServices) {
+        return null;  // stub
+    }
+
+    // EFFECTS: returns set of resources in registry that off any of the services in requestedServices set
+    public Set<Resource> getResourcesOfferingAnyServicesInSet(Set<Service> requestedServices) {
+        return null;  // stub
+    }
+}
